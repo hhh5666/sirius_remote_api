50d55ce2678e8d67563067f4f675508cd9ac4204
hhh5666 <69581221+hhh5666@users.noreply.github.com>
2025-07-12 16:12:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from datetime import date
 import json
 import os
 
@@ -26,5 +27,23 @@ def add_place():
     data = request.get_json()
     places = load_places()
     places.append(data)
+    save_places(places)
+    return jsonify({'status': 'success'})
+
+@app.route('/add_review', methods=['POST'])
+def add_review():
+    data = request.get_json()
+    place_name = data['place']
+    new_review = {
+        'rating': data['rating'],
+        'comment': data['comment'],
+        'date': str(date.today())
+    }
+
+    places = load_places()
+    for place in places:
+        if place['name'] == place_name:
+            place.setdefault('reviews', []).append(new_review)
+            break
     save_places(places)
     return jsonify({'status': 'success'})
